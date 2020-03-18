960b964c05002b4f27558d93388f9b92593a8bd4
Leslie <leslie.appl@gmail.com>
2020-03-18 18:16:55 +0800

    Initial Commit

---
+++ TabBarControllerAllen/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabBarControllerAllen
+//
+//  Created by leslie on 3/18/20.
+//  Copyright © 2020 leslie. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabBarControllerAllen/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabBarControllerAllen
+//
+//  Created by leslie on 3/18/20.
+//  Copyright © 2020 leslie. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
